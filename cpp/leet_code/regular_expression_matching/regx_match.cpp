ef3c8a33905b0b88264e155f913322fab6a82298
yanbo <yanbo@letv.com>
2017-04-12 19:55:00 +0800

    test

--- cpp/leet_code/regular_expression_matching/regx_match.cpp
+++ cpp/leet_code/regular_expression_matching/regx_match.cpp
@@ -45,20 +45,27 @@ public:
 
           if(size_p == 0)
           {
-              if(size_s == 0)
-              {
-                    return true;
-              }
-          	 return false;
+                if(size_s == 0)
+                {
+                      return true;
+                }
+            	 return false;
           }
           if(size_p == 1)
           {
-               if(size_s ==1 && (s[0] == p[0] || '.' == p[0]))
-               {
-                     return true;
-               }
+                 if(size_s ==1 && (s[0] == p[0] || '.' == p[0]))
+                 {
+                       return true;
+                 }
 
-               return false;
+                 return false;
+          }
+          if(p[size_p -1]!='*' && p[size_p -1]!='.')
+          {
+                  if(size_s > 0 && s[size_s-1] != p[size_p-1])
+                  {
+                        return false;
+                  }
           }
 
            if(p[1] == '*')
@@ -66,24 +73,25 @@ public:
 
                 if(size_s >0 && (s[0] == p[0] || '.' == p[0]))
                 {
-                    if (isMatch(s +1 , p))
+                      if (isMatch(s +1 , p+2))
                       {
-                          return true;
+                            return true;
                       }
-                     if(isMatch(s, p +2))
-                     {
-                          return true;
-                     }
 
-                     if(isMatch(s +1, p +2))
-                     {
-                          return true;
-                     }
-                     return false;
+                       if(isMatch(s , p +2))
+                       {
+                            return true;
+                       }
+
+                       if(isMatch(s +1, p ))
+                       {
+                            return true;
+                       }
+                      return false;
                 }
                 else
                 {
-                    return  isMatch(s, p +2);
+                      return  isMatch(s, p +2);
                 }
             }
             else
@@ -104,71 +112,71 @@ public:
 
 TEST_CASE("Testing Regx Matching") {
 	    // set up
-	     Solution sol;
-	    string s = "abcde";
-	    string p = "*";
+            Solution sol;
+            string s = "abcde";
+            string p = "*";
 
 	    // different sections
  
-          SECTION("abcde matches .*e") {
-              s = "abcde";
-              p = ".*e";
-              REQUIRE(sol.isMatch(s, p) );
-          }
+            SECTION("abcde matches .*e") {
+                s = "abcde";
+                p = ".*e";
+                REQUIRE(sol.isMatch(s, p) );
+            }
 
          
-	    SECTION("abcde does not matche *") {
+              SECTION("abcde does not matche *") {
 	        s = "abcde";
 	        p = "*";
 	        REQUIRE(!sol.isMatch(s, p) );
-	    }
+              }
 
-	    SECTION("abcde does not match .*c") {
+	       SECTION("abcde does not match .*c") {
 	        s = "abcde";
 	        p = ".*c";
 	        REQUIRE(!sol.isMatch(s, p) );
-	    }
+	      }
 
-          SECTION("aab  matches  c*a*b") {
-          s = "aab";
-          p = "c*a*b";
-          REQUIRE(sol.isMatch(s, p) );
-       }
+              SECTION("aab  matches  c*a*b") {
+                s = "aab";
+                p = "c*a*b";
+                REQUIRE(sol.isMatch(s, p) );
+             }
 
-          SECTION("aa matches a*") {
-          s = "aa";
-          p = "a*";
-          REQUIRE(sol.isMatch(s, p) );
-       }
+             SECTION("aa matches a*") {
+                  s = "aa";
+                  p = "a*";
+                 REQUIRE(sol.isMatch(s, p) );
+           }
 
-          SECTION("aab doesn't matches a*") {
-          s = "aab";
-          p = "a*";
-          REQUIRE(!sol.isMatch(s, p) );
-       }
+              SECTION("aab doesn't matches a*") {
+                  s = "aab";
+                  p = "a*";
+                  REQUIRE(!sol.isMatch(s, p) );
+             }
  
-         SECTION("a matches ab*") {
-          s = "a";
-          p = "ab*";
-          REQUIRE(sol.isMatch(s, p) );
-       }
+              SECTION("a matches ab*") {
+                    s = "a";
+                    p = "ab*";
+                    REQUIRE(sol.isMatch(s, p) );
+              }
 
-          SECTION("bbba matches .*a*a") {
-          s = "bbba";
-          p = ".*a*a";
-          REQUIRE(sol.isMatch(s, p) );
-       }
+              SECTION("bbba matches .*a*a") {
+                    s = "bbba";
+                    p = ".*a*a";
+                    REQUIRE(sol.isMatch(s, p) );
+             }
 
-         SECTION("a does not match .*..a*") {
-          s = "a";
-          p = ".*..a*";
-          REQUIRE(!sol.isMatch(s, p) );
-       }
+            SECTION("a does not match .*..a*") {
+                  s = "a";
+                  p = ".*..a*";
+                  REQUIRE(!sol.isMatch(s, p) );
+            }
 
-        SECTION("aaaaaaaaaaaaab does not match a*a*a*a*a*a*a*a*a*a*c") {
-          s = "aaaaaaaaaaaaab";
-          p = "a*a*a*a*a*a*a*a*a*a*c";
-          REQUIRE(!sol.isMatch(s, p) );
-       }
+              SECTION("aaaaaaaaaaaaab does not match a*a*a*a*a*a*a*a*a*a*c") {
+                    s = "aaaaaaaaaaaaab";
+                    p = "a*a*a*a*a*a*a*a*a*a*c";
+                    REQUIRE(!sol.isMatch(s, p) );
+             }
        
    }
